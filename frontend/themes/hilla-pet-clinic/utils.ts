91f76b8250411d55a08cb0310045d0517440e890
Ahsin Siddique <ahsin.siddique@gmail.com>
2023-07-13 13:16:38 +0800

    fix date null check format validation

--- frontend/themes/hilla-pet-clinic/utils.ts
+++ frontend/themes/hilla-pet-clinic/utils.ts
@@ -1,6 +1,7 @@
 import dateFnsFormat from 'date-fns/format';
 
 export const formatDateIso8601 = (dateParts: any): string => {
+    if (dateParts == "") return "";
     const date = new Date(dateParts);
     return dateFnsFormat(date, 'yyyy-MM-dd');
 };
