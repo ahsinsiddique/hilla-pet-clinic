1ebd41e2a8eb7c9d1b36238f182f17bfb1b08cf8
Ahsin Siddique <ahsin.siddique@gmail.com>
2023-08-14 21:30:55 +0800

    add main layout complexity fixes, and active tab fix

--- frontend/views/MainLayout.tsx
+++ frontend/views/MainLayout.tsx
@@ -3,9 +3,11 @@ import Placeholder from 'Frontend/components/placeholder/Placeholder.js';
 import { MenuProps, routes, useViewMatches, ViewRouteObject } from 'Frontend/routes.js';
 import { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import css from './MainLayout.module.css';
 import { Tab } from "@hilla/react-components/Tab.js";
 import { Tabs } from "@hilla/react-components/Tabs.js";
+import { Icon } from "@hilla/react-components/Icon.js";
+import '@vaadin/icons'
+import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
 
 type MenuRoute = ViewRouteObject &
     Readonly<{
@@ -15,33 +17,22 @@ type MenuRoute = ViewRouteObject &
 
 export default function MenuOnLeftLayout() {
     const matches = useViewMatches();
-    const menuRoutes = (routes[0]?.children || []).filter(
-        (route) => route.path && route.handle && route.handle.icon && route.handle.title
-    ) as readonly MenuRoute[];
+    const menuRoutes = (routes[0]?.children || []) as readonly MenuRoute[];
 
     return (
         <AppLayout>
 
             <h3 style={{ marginLeft: "10rem" }} slot="navbar">
-                <img src="../themes/hilla-logo.png" alt="" style={{ width: "25px", paddingRight: "1rem" }} />Hilla Pet Clinic</h3>
+                <img src="../themes/hilla-logo.png" alt="" style={{ width: "25px", paddingRight: "1rem" }} />
+                Hilla Pet Clinic
+            </h3>
+
             <Tabs slot="navbar" orientation="horizontal">
                 {menuRoutes.map(({ path, handle: { icon, title, isNotDisplayOnNavBar } }) => (
-                    < Tab key={path} >
-                        {!isNotDisplayOnNavBar && <NavLink
-                            className={({ isActive }) => `${css.navlink} ${isActive ? css.navlink_active : ''}`}
-                            key={path}
-                            to={path}>
-                            <span
-                                className={css.navicon}
-                                style={
-                                    {
-                                        '--mask-image': `url('line-awesome/svg/${icon}.svg')`,
-                                        maskImage: 'var(--mask-image)',
-                                        WebkitMaskImage: 'var(--mask-image)',
-                                    } as any
-                                }
-                                aria-hidden="true"
-                            ></span>{title}
+                    < Tab key={path} > {!isNotDisplayOnNavBar &&
+                        <NavLink className={({ isActive }) => `${isActive ? 'bg-contrast-10' : ''}`}
+                            key={path} to={path}>
+                            <Icon icon={icon} />  {title}
                         </NavLink>}
                     </Tab>
                 ))}
